1b2d2835de56117e00367bd80a862bf0a6a4340f
toel-kamanda34 <toelokemwa@gmail.com>
2024-09-16 20:03:41 +0300

    completed lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears ( name,age,sex,color,temperament,alive) VALUES("Mr. Chocolate", 5,"M","Brown","Maternal",1);
+INSERT INTO bears ( name,age,sex,color,temperament,alive) VALUES("Rowdy", 7,"M","Black","Territorial",1);
+INSERT INTO bears ( name,age,sex,color,temperament,alive) VALUES("Tabitha", 9,"F","Brown","Opportunistic",1);
+INSERT INTO bears ( name,age,sex,color,temperament,alive) VALUES("Sergeant Brown", 12,"F","Chocolate","Docile",1);
+INSERT INTO bears ( name,age,sex,color,temperament,alive) VALUES("Melissa", 32, "F","White","Playful",1);
+INSERT INTO bears ( name,age,sex,color,temperament,alive) VALUES("Grinch", 23,"M","Dark Brown","Aggressive",1);
+INSERT INTO bears ( name,age,sex,color,temperament,alive) VALUES("Wendy", 43, "F","Brown","Shy",1);
+INSERT INTO bears ( name,age,sex,color,temperament,alive) VALUES(NULL , 1,"M","White","Curious",0);
